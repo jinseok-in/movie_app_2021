f8d1f90be97d3eefbbcf530a85b7596283e8c41c
jinseok-in <wlstjr1964@naver.com>
2021-06-26 16:03:07 +0900

    210626 commit

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({name}){
+  return <h1>I like {name}</h1>;
+}
+
+const foodILike = [
+  {id:1,name : "Kimchi"},
+  {id:2,name : "Bibimbap"},
+  {id:3,name : "Samgyeopsal"}
+];
+
+function renderFood(dish){
+  return <Food name={dish.name} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name}/>
+      ))}
     </div>
-  );
+    );
 }
 
 export default App;
